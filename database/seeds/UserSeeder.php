4f1fe6f89f88269a10b4a6588d304b73cfef2e69
Luis Vargas <lewiis29@gmail.com>
2020-10-09 17:13:03 -0600

    Construcción de consultas SQL con Laravel Seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+//        $professions = DB::select('SELECT id FROM professions WHERE title = ?', ['Desarrollador back-end']);
+
+        $professionId = DB::table('professions')
+            ->where('title', 'Desarrollador back-end')
+            ->value('id');
+
+
         DB::table('users')->insert([
             'name'=>'Luis Vargas',
             'email'=>'luis@styde.net',
             'password'=>bcrypt('123'),
+            'profession_id'=>$professionId
         ]);
     }
 }
